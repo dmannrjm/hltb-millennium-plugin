124243a84632ae11d58575ac0eaea1d392c11148
Joey Doll <jcdoll@gmail.com>
2026-01-17 09:35:53 -0800

    remove user submitted no-ops and add missing comma

--- backend/name_fixes.lua
+++ backend/name_fixes.lua
@@ -26,15 +26,9 @@ return {
     ["Life Goes On: Done to Death"] = "Life Goes On",
     ["Meadow - A Shelter Game"] = "Meadow",
     ["Warhammer 40,000: Dawn of War II: Retribution"] = "Warhammer 40,000: Dawn of War II - Retribution",
-    ["STAR WARS Battlefront II (Classic, 2005)"] = "STAR WARS Battlefront II"
+    ["STAR WARS Battlefront II (Classic, 2005)"] = "STAR WARS Battlefront II",
     ["Marvel’s Spider-Man Remastered"] = "Marvel's Spider-Man Remastered",
-    ["Disco Elysium - The Final Cut"] = "Disco Elysium",
     ["Lewd Girls, Leave Me Alone! I Just Want to Play Video Games and Watch Anime! ~ Hentai Edition"] = "Lewd Girls, Leave Me Alone! I Just Want to Play Video Games and Watch Anime! Hentai Edition",
-    ["PICO PARK:Classic Edition"] = "PICO PARK: Classic Edition",
     ["Ranch Simulator: Build, Hunt, Farm"] = "Ranch Simulator",
     ["Sea of Thieves: 2025 Edition"] = "Sea of Thieves",
-    ["Stardew Valley"] = "Stardew Valley",
-    ["Baldur's Gate 3"] = "Baldur's Gate 3",
-    ["Vampire Survivors"] = "Vampire Survivors",
-    ["The Deed: Dynasty"] = "The Deed: Dynasty",
 }
